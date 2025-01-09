05f0d8b47636d9ab862945b90431a37ad773380a
NguyenVanLoi2004 <loishy2004@gmail.com>
2025-01-09 20:05:25 +0700

    fix bug lần N

--- src/User/views/TableTask.jsx
+++ src/User/views/TableTask.jsx
@@ -250,7 +250,8 @@ const TableTask = () => {
             btnup.click()
 
           }} className="bi bi-pencil-square edit-icon"></i>
-          <i  onClick={()=>{
+          <i  onClick={(event)=>{
+              event.stopPropagation();
              API_Delete_Task(task._id)
           }}  className="bi bi-trash delete-icon"></i>
         </div>
@@ -344,7 +345,7 @@ const TableTask = () => {
         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
       </div>
      
-      <div className="modal-body" style={{ backgroundColor: '#f8f9fa', padding: '10px', borderRadius: '15px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', minWidth: '800px', margin: '0 auto' }}>
+      <div className="modal-body" style={{ backgroundColor: '#f8f9fa', padding: '10px', borderRadius: '15px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', maxWidth: '800px', margin: '0 auto' }}>
   {/* Job Header */}
   <div style={{ backgroundColor: '#007bff', color: 'white', borderRadius: '15px 15px 0 0', padding: '20px' }}>
     <h3 style={{ fontSize: '24px', fontWeight: 'bold' }}>{TaskDetail.Title}</h3>
