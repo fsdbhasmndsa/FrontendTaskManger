8f0f7532f521af6e0ecd17e0bec0a652119baef2
NguyenVanLoi2004 <loishy2004@gmail.com>
2025-01-12 00:09:42 +0700

    update responsive

--- src/User/Layout/Layout.jsx
+++ src/User/Layout/Layout.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import Sidebar from '../partials/Sidebar'
-import Header from '../partials/Header'
-import { Outlet } from 'react-router-dom'
+import React from 'react';
+import Sidebar from '../partials/Sidebar';
+import Header from '../partials/Header';
+import { Outlet } from 'react-router-dom';
 
 const Layout = () => {
   return (
     <div className="d-flex">
-    {/* Sidebar */}
-    <Sidebar />
+      {/* Sidebar */}
+      <Sidebar />
 
-    {/* Main Content */}
-    <div className="flex-grow-1">
-      {/* Header */}
-      <Header />
+      {/* Main Content */}
+      <div className="flex-grow-1">
+        {/* Header */}
+        <Header />
 
-      {/* Nội dung động hiển thị qua Outlet */}
-      <div className="p-4">
-        <Outlet />
+        {/* Dynamic Content */}
+        <div className="p-4">
+          <Outlet />
+        </div>
       </div>
     </div>
-  </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
