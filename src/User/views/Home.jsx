25c1a8e2060961dac5c0ca246236f7eb28dcd05a
NguyenVanLoi2004 <loishy2004@gmail.com>
2025-01-11 23:28:17 +0700

    sữa lại PrivateRoute "

--- src/User/views/Home.jsx
+++ src/User/views/Home.jsx
@@ -7,16 +7,16 @@ const Home = () => {
       <div className="row flex-grow-1">
         {/* Cột bên trái */}
         <div className="col-6 d-flex flex-column justify-content-center align-items-start p-5" style={{ background: `linear-gradient(87deg, #11cdef, #1171ef)`}}>
-          <h1 className="text-light fw-bold display-4">Brainstorm names</h1>
-          <p className="fs-4">for my fantasy football team</p>
+          <h1 className="text-light fw-bold display-4">Task Mangagement</h1>
+          <p className="fs-4">Specify your goals and tasks</p>
         </div>
 
         {/* Cột bên phải */}
         <div className="col-6 d-flex flex-column justify-content-center align-items-center bg-white">
           <h2 className="fw-bold mb-4">Get started</h2>
           <div>
-            <NavLink to={"/login"} className="btn btn-primary me-3 px-4 py-2">Log in</NavLink>
-            <NavLink to={"/register"} className="btn btn-outline-primary px-4 py-2">Sign up</NavLink>
+            <NavLink to={"/login"} className="btn btn-primary me-3 px-4 py-2 my-2">Đăng nhập</NavLink>
+            <NavLink to={"/register"} className="btn btn-outline-primary px-4 py-2 my-2">Đăng kí</NavLink>
           </div>
         </div>
       </div>
--- src/User/views/PrivateRoute.jsx
+++ src/User/views/PrivateRoute.jsx
@@ -1,25 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 const PrivateRoute = ({ element }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(null); // null: chưa xác định, true/false: đã xác định
-
+  const navigate = useNavigate();
+  
+  const token = localStorage.getItem('Token') || null;
   useEffect(() => {
-    const token = localStorage.getItem('Token');
-    if (token) {
-      setIsAuthenticated(true); // Nếu có token, xác thực thành công
-    } else {
-      setIsAuthenticated(false); // Nếu không có token, xác thực thất bại
-    }
-  }, []);
+   
+    if (token == null) {
+      navigate("/login")
+    } 
+  }, [token,navigate]);
 
-  if (isAuthenticated === null) {
-    // Chờ kiểm tra xác thực trước khi quyết định render
-    return <div>Loading...</div>;
-  }
 
-  // Điều hướng đến login nếu không xác thực
-  return isAuthenticated ? element : <Navigate to="/login" replace />;
+
+  
+  if (token == null) {
+    return null; // Không render gì
+}
+    
+
+ return element
 };
 
 export default PrivateRoute;
